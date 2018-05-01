d1f0879acdbc76ee09b7b96b8a34ba960daa52ab
Sascha Sägesser <sascha.saegesser@stud.hslu.ch>
2018-05-01 18:59:05 +0200

    Navigation funktionsfähig gemacht

--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
+$(document).ready(function() {
+  loadDoc('uebersicht');
+})
+
+// Content mittels AJAX reinladen
 function loadDoc(fileName) {
-    $("#content").load("content/" + fileName + ".html");
+  $("#content-container").load("content/" + fileName + ".html");
 }
 
+// Navigation for mobile view
+function dropdownNav() {
+  var x = document.getElementById("topnav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
+
+// Aktive Seite hervorheben
+var navContainer = null;
+var navs = null;
 $(document).ready(function() {
-    loadDoc('uebersicht');
+  navContainer = document.getElementById("topnav");
+  navs = navContainer.getElementsByClassName("navBtn");
+
+  // EventListener für jedes Navigationselement hinzufügen
+  for (var i = 0; i < navs.length; i++) {
+    navs[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active"; 
+      }
+    });
+  } 
 })
