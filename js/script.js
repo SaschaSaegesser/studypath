fc8e368a680f1d0f37f0e04fd7a549a6b905eaed
Sascha Sägesser <sascha.saegesser@stud.hslu.ch>
2018-05-22 19:36:03 +0200

    Einschreibung implementiert

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function loadDoc(fileName) {
 
 $(document).ready(function () {
   "use strict";
-  loadDoc('faq');
+  loadDoc('einschreibung');
 });
 
 // Navigation for mobile view
@@ -79,3 +79,28 @@ window.onclick = function (event) {
     document.getElementById('myModal').style.display = "none";
   }
 };
+
+// Drag & Drop-Funktion für die Module der Einschreibung
+function dragStart(ev) {
+  ev.dataTransfer.effectAllowed = 'move';
+  ev.dataTransfer.setData("Text", ev.target.getAttribute('id'));
+  ev.dataTransfer.setDragImage(ev.target,0,0);
+  return true;
+}
+
+function dragEnter(ev) {
+  event.preventDefault();
+  return true;
+}
+
+function dragOver(ev) {
+  return false;
+}
+
+function dragDrop(ev) {
+  var src = ev.dataTransfer.getData("Text");
+  ev.target.appendChild(document.getElementById(src));
+  ev.stopPropagation();
+  return false;
+}
+
